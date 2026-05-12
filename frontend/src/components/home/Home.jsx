0d019bf2df3f1ae4dd75bb5fa83694d589fb7da2
sythe <188770367+sythoniq@users.noreply.github.com>
2026-05-12 18:57:39 +0300

    added some styling to have a slightly workable with version of the page

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -1,13 +1,21 @@
 import { Link } from 'react-router'
 import game from '../../assets/ww-1.jpg';
 
+import "./Home.style.css"
+
 export default function Home() {
   return (
     <main className="home-page">
-      <div className="game-images">
-        <img src={game} alt="Game image for where's waldo thingy" />
-      </div>
-      <Link to="/game"><button>Start Game</button></Link>
+        <div className="game-images">
+          <Link to="/images/1">
+            <div className="game-image">
+              <img src={game} alt="Game image for where's waldo thingy" />
+              <div className="game-details">
+                <h3>Image 1</h3>
+              </div>
+            </div>
+          </Link>
+        </div>
     </main>
   )
 }
